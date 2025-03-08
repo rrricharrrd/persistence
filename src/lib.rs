d097a21d0f331492bf8390b9974494bba74c5956
Richard Harris <rrricharrrd@gmail.com>
2025-03-08 21:50:16 +0100

    MAINT: store filtration level with simplex

--- src/lib.rs
+++ src/lib.rs
@@ -6,16 +6,15 @@ use log::debug;
 #[derive(Clone, Debug)]
 struct Entry {
     simplex: Simplex,
-    filtration_level: usize,
-    is_marked: bool,
-    co_bounds: HashSet<usize>,
-    //bounding_chain: Some(usize),
+    is_marked: bool, // Is cycle to be retained in next dimension
+    co_bounds: HashSet<usize>, // Elements of pivot column
 }
 
 /// Simplex (as defined via its vertices)
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
 pub struct Simplex {
     vertices: Vec<usize>,
+    filtration_level: usize,
 }
 
 impl Simplex {
@@ -30,6 +29,7 @@ fn remove_pivot_rows(
     table: &[Entry],
 ) -> HashSet<usize> {
     // Get boundary indices of given simplex
+    let simplex = table[simplex_ix].simplex.clone();
     let mut boundary: HashSet<usize> = boundary_op
         .iter()
         .enumerate()
@@ -44,18 +44,20 @@ fn remove_pivot_rows(
     boundary.retain(|&bx| table[bx].is_marked);
     debug!(
         "Removing pivot from {:?}, full-boundary={:?}",
-        table[simplex_ix].simplex,
+        simplex,
         boundary
     );
 
     // Simulate conversion to echelon form
     while let Some(b) = boundary.clone().into_iter().max() {
         if table[b].co_bounds.is_empty() {
+            // This row is unclaimed - use as pivot
             break;
         } else {
             for cb in &table[b].co_bounds {
+                // Simulate subtracting pivot column (working over Z/2)
                 if boundary.contains(cb) {
-                    boundary.remove(cb); // Working over Z/2
+                    boundary.remove(cb);
                 } else {
                     boundary.insert(*cb);
                 }
@@ -65,10 +67,9 @@ fn remove_pivot_rows(
 
     debug!(
         "Removing pivot from {:?}: reduced-boundary={:?}",
-        table[simplex_ix].simplex,
+        simplex,
         boundary
     );
-
     boundary
 }
 
@@ -76,7 +77,6 @@ fn remove_pivot_rows(
 /// Compute persistence intervals for simplicial complex
 pub fn compute_intervals(
     simplices: &[Simplex],
-    simplices_map: &HashMap<Simplex, usize>,
     boundary_op: &[Vec<i32>],
 ) -> Vec<HashSet<(usize, usize)>> {
     let mut table: Vec<Entry> = simplices
@@ -84,7 +84,6 @@ pub fn compute_intervals(
         .map(|s| {
             Entry {
                 simplex: s.clone(),
-                filtration_level: *simplices_map.get(s).unwrap(),
                 is_marked: false,
                 co_bounds: HashSet::new(),
             }
@@ -104,7 +103,10 @@ pub fn compute_intervals(
             table[b].co_bounds = boundary.clone();
 
             let dim = table[b].simplex.dim();
-            intervals[dim].insert((table[b].filtration_level, table[sx].filtration_level));
+            intervals[dim].insert((
+                table[b].simplex.filtration_level,
+                table[sx].simplex.filtration_level,
+            ));
         }
     }
 
@@ -113,7 +115,8 @@ pub fn compute_intervals(
         debug!("{:?}", entry);
         if entry.is_marked && entry.co_bounds.is_empty() {
             let dim = entry.simplex.dim();
-            intervals[dim].insert((entry.filtration_level, usize::MAX)); // usize::MAX for infinity
+            // Note: usize::MAX for infinity
+            intervals[dim].insert((entry.simplex.filtration_level, usize::MAX));
         }
     }
     intervals
@@ -123,22 +126,37 @@ pub fn compute_intervals(
 /// Compute boundary operator for given simplicial complex
 pub fn compute_boundary_op(simplices: &Vec<Simplex>) -> Vec<Vec<i32>> {
     let n = simplices.len();
+    // TODO throughout using dummy filtration level, as not needed to define boundary matrix
     let ordering: HashMap<Simplex, usize> = simplices
         .iter()
         .enumerate()
-        .map(|(i, v)| (v.clone(), i))
+        .map(|(i, v)| {
+            (
+                Simplex {
+                    vertices: v.vertices.clone(),
+                    filtration_level: 0,
+                },
+                i,
+            )
+        })
         .collect();
 
-    let mut boundary_op = vec![vec![0; n]; n]; // Initialize zero matrix
-
+    let mut boundary_op = vec![vec![0; n]; n];
     for s in simplices {
+        let ss = Simplex {
+            vertices: s.vertices.clone(),
+            filtration_level: 0,
+        }; // TODO
         for &v in &s.vertices {
             let mut ds_vertices = s.vertices.clone();
             ds_vertices.retain(|&x| x != v); // Remove vertex
-            let ds = Simplex { vertices: ds_vertices };
+            let ds = Simplex {
+                vertices: ds_vertices,
+                filtration_level: 0,
+            };
 
             if let Some(&row) = ordering.get(&ds) {
-                if let Some(&col) = ordering.get(s) {
+                if let Some(&col) = ordering.get(&ss) {
                     boundary_op[row][col] = 1;
                 }
             }
@@ -158,32 +176,56 @@ mod tests {
         let _ = env_logger::try_init();
 
         let simplices = vec![
-            Simplex { vertices: vec![0] },
-            Simplex { vertices: vec![1] },
-            Simplex { vertices: vec![2] },
-            Simplex { vertices: vec![3] },
-            Simplex { vertices: vec![0, 1] },
-            Simplex { vertices: vec![1, 2] },
-            Simplex { vertices: vec![2, 3] },
-            Simplex { vertices: vec![0, 3] },
-            Simplex { vertices: vec![0, 2] },
-            Simplex { vertices: vec![0, 1, 2] },
-            Simplex { vertices: vec![0, 2, 3] },
+            Simplex {
+                vertices: vec![0],
+                filtration_level: 0,
+            },
+            Simplex {
+                vertices: vec![1],
+                filtration_level: 0,
+            },
+            Simplex {
+                vertices: vec![2],
+                filtration_level: 1,
+            },
+            Simplex {
+                vertices: vec![3],
+                filtration_level: 1,
+            },
+            Simplex {
+                vertices: vec![0, 1],
+                filtration_level: 1,
+            },
+            Simplex {
+                vertices: vec![1, 2],
+                filtration_level: 1,
+            },
+            Simplex {
+                vertices: vec![2, 3],
+                filtration_level: 2,
+            },
+            Simplex {
+                vertices: vec![0, 3],
+                filtration_level: 2,
+            },
+            Simplex {
+                vertices: vec![0, 2],
+                filtration_level: 3,
+            },
+            Simplex {
+                vertices: vec![0, 1, 2],
+                filtration_level: 4,
+            },
+            Simplex {
+                vertices: vec![0, 2, 3],
+                filtration_level: 5,
+            },
         ];
-        let levels = vec![0, 0, 1, 1, 1, 1, 2, 2, 3, 4, 5];
-        assert_eq!(simplices.len(), levels.len());
-
-        let simplices_map: HashMap<Simplex, usize> = simplices
-            .clone()
-            .into_iter()
-            .zip(levels.into_iter())
-            .collect();
-        debug!("Simplices {:?}", simplices_map);
 
         let boundary_op: Vec<Vec<i32>> = compute_boundary_op(&simplices);
         debug!("Boundary {:?}", boundary_op);
 
-        let result = compute_intervals(&simplices, &simplices_map, &boundary_op);
+        let result = compute_intervals(&simplices, &boundary_op);
         debug!("Result {:?}", result);
 
         let expected = vec![
