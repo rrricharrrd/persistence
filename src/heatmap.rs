1666870cf17be92d58541e75e37a533fb2e1492b
Richard Harris <rrricharrrd@gmail.com>
2025-03-16 16:01:31 +0100

    MAINT: start building complex from heatmap

--- src/heatmap.rs
+++ src/heatmap.rs
@@ -1,11 +1,22 @@
+use super::union_find::UnionFind;
+
+
 #[derive(Debug, Clone)]
 pub struct Heatmap {
     pub pixels: Vec<Vec<f64>>,
 }
 
 impl Heatmap {
+    fn height(&self) -> usize {
+        self.pixels.len()
+    }
+
+    fn width(&self) -> usize {
+        self.pixels[0].len()
+    }
+
     #[allow(dead_code)] // TODO
-    fn sorted_pixel_indices(&self) -> Vec<(usize, usize)> {
+    fn sorted_pixel_indices(&self) -> Vec<(f64, usize, usize)> {
         let mut pixels: Vec<(f64, usize, usize)> = Vec::new();
 
         for (i, row) in self.pixels.iter().enumerate() {
@@ -23,7 +34,49 @@ impl Heatmap {
         );
 
         // Extract just (i, j) pairs
-        pixels.into_iter().map(|(_, i, j)| (i, j)).collect()
+        // pixels.into_iter().map(|(_, i, j)| (i, j)).collect()
+        pixels
+    }
+
+    #[allow(dead_code)] // TODO
+    fn find_maxima(&self) -> UnionFind {
+        let pixels = self.sorted_pixel_indices();
+        let uf = UnionFind::new(pixels.len());
+        for (v, i, j) in pixels {
+            // let index = i * self.width() + j;
+
+            let up = if i > 0 {
+                self.pixels[i - 1][j]
+            } else {
+                -f64::INFINITY
+            };
+            let down = if i < self.height() - 1 {
+                self.pixels[i + 1][j]
+            } else {
+                -f64::INFINITY
+            };
+            let is_ymin = up > v && down > v;
+            let is_ymax = up < v && down < v;
+
+            let left = if j > 0 {
+                self.pixels[i][j - 1]
+            } else {
+                -f64::INFINITY
+            };
+            let right = if j < self.width() - 1 {
+                self.pixels[i][j + 1]
+            } else {
+                -f64::INFINITY
+            };
+            let is_xmin = left > v && right > v;
+            let is_xmax = left < v && right < v;
+
+            let _is_max = is_xmax && is_ymax;
+            let _is_min = is_xmin && is_ymin;
+            let _is_saddle = (is_xmax && is_ymin) || (is_xmin && is_ymax);
+            // TODO
+        }
+        uf
     }
 }
 
@@ -36,15 +89,12 @@ mod tests {
     fn test_union_find() {
         let _ = env_logger::try_init();
 
-        let heatmap = Heatmap {
-            pixels: vec![
-                vec![0.2, 0.8, 0.5],
-                vec![0.4, 0.1, 0.9],
-                vec![0.7, 0.3, 0.6],
-            ],
-        };
+        let heatmap = Heatmap { pixels: vec![vec![0.2, 0.8, 0.5], vec![0.4, 0.1, 0.9]] };
+        assert_eq!(heatmap.height(), 2);
+        assert_eq!(heatmap.width(), 3);
 
         let sorted_indices = heatmap.sorted_pixel_indices();
+        assert_eq!(sorted_indices[0], (0.9, 1, 2));
         debug!("{:?}", sorted_indices);
     }
 }
